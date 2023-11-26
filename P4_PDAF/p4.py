stopgap-fix pdaf covariance update bug, clean plotting legends

P4_PDAF/p4.py
@@ -16,7 +16,7 @@ import numpy as np
 import pandas as pd
 import math
 from matplotlib import pyplot as plt
-from p4_Classes import EKF, NN
+from p4_Classes import EKF, NN, PDAF
 import os
 import copy
 
@@ -52,9 +52,12 @@ P0 = np.diag([sigma_x**2, sigma_y**2, sigma_xdot **
              2, sigma_ydot**2, sigma_w**2])
 ekf_clean = EKF(x0, P0)
 ekf_clutter = EKF(copy.deepcopy(x0), copy.deepcopy(P0))
+pdaf_clean = PDAF(copy.deepcopy(x0), copy.deepcopy(P0))
+pdaf_clutter = PDAF(copy.deepcopy(x0), copy.deepcopy(P0))
 dt = 1/1  # measurements taken at 1 Hz
 
 for i in range(datalength):
+    print(i)
 
     # run clean EKF
     #print(f'{i} clean/cluttered:')
@@ -68,7 +71,12 @@ for i in range(datalength):
     xs_clutterEKF[i,:] = NN.runNNEKF(ekf_clutter, dt, ranges_clutter, bearings_clutter)
     
     # run clean PDAF
+    xs_cleanPDAF[i,:] = pdaf_clean.runPDAF(dt, ranges_clean, bearings_clean)
+    #print(pdaf_clean.p_hat-ekf_clean.p_hat)
 
+    # run cluttered PDAF
+    xs_clutterPDAF[i,:] = pdaf_clutter.runPDAF(dt, ranges_clutter, bearings_clutter)
+    
 
 
 # plot results for cluttered sensor data with missed detections:
@@ -88,6 +96,9 @@ ax.plot(dataframes['truth'].iloc[:, 0],
 ax.scatter(measures[:,0], measures[:,1], marker='o',s=1, color='b')
 ax.plot(xs_cleanEKF[:,0], xs_cleanEKF[:,1], 'b', label='clean NN EKF')
 ax.plot(xs_clutterEKF[:,0], xs_clutterEKF[:,1], 'g', label='cluttered NN EKF')
+ax.plot(xs_cleanPDAF[:,0],xs_cleanPDAF[:,1], 'c', label='clean PDAF')
+ax.plot(xs_clutterPDAF[:,0],xs_clutterPDAF[:,1], 'y', label='cluttered PDAF')
+
 ax.set(xlabel = 'x, m', ylabel = 'y, m',
       title = 'xy plane track trajectory')
 ax.legend()
@@ -100,12 +111,14 @@ fig.suptitle("Position vs time")
 ax1.plot(dataframes['truth'].iloc[:, 0], 'r', label='truth')
 ax1.plot(xs_clutterEKF[:,0], 'g', label='cluttered NN EKF')
 ax1.plot(xs_clutterPDAF[:,0], 'b', label='cluttered PDAF')
+ax1.plot(xs_cleanPDAF[:,0], 'c', label='clean PDAF')
 ax1.set(xlabel="Time (s)")
 ax1.set(ylabel="x_pos (m)")
 
-ax2.plot(dataframes['truth'].iloc[:, 2], 'r', label='truth')
-ax2.plot(xs_clutterEKF[:,1], 'g', label='cluttered NN EKF')
-ax1.plot(xs_clutterPDAF[:,1], 'b', label='cluttered PDAF')
+ax2.plot(dataframes['truth'].iloc[:, 2], 'r')
+ax2.plot(xs_clutterEKF[:,1], 'g')
+ax2.plot(xs_clutterPDAF[:,1], 'b')
+ax2.plot(xs_cleanPDAF[:,1], 'c')
 ax2.plot()
 ax2.set(xlabel="Time (s)")
 ax2.set(ylabel="y_pos (m)")
@@ -117,12 +130,14 @@ fig.suptitle("Velocity vs time")
 ax1.plot(dataframes['truth'].iloc[:, 1], 'r', label='truth')
 ax1.plot(xs_clutterEKF[:,2], 'g', label='cluttered NN EKF')
 ax1.plot(xs_clutterPDAF[:,2], 'b', label='cluttered PDAF')
+ax1.plot(xs_cleanPDAF[:,2], 'c', label='clean PDAF')
 ax1.set(xlabel="Time (s)")
 ax1.set(ylabel="v_x (m/s)")
 
-ax2.plot(dataframes['truth'].iloc[:, 3], 'r', label='truth')
-ax2.plot(xs_clutterEKF[:,3], 'g', label='cluttered NN EKF')
-ax1.plot(xs_clutterPDAF[:,3], 'b', label='cluttered PDAF')
+ax2.plot(dataframes['truth'].iloc[:, 3], 'r')
+ax2.plot(xs_clutterEKF[:,3], 'g' )
+ax2.plot(xs_clutterPDAF[:,3], 'b')
+ax2.plot(xs_cleanPDAF[:,3], 'c')
 ax2.plot()
 ax2.set(xlabel="Time (s)")
 ax2.set(ylabel="v_y (m/s)")
@@ -134,5 +149,7 @@ fig.suptitle("Turn rate vs time")
 ax1.plot(dataframes['truth'].iloc[:, 4], 'r', label='truth')
 ax1.plot(xs_clutterEKF[:,4], 'g', label='cluttered NN EKF')
 ax1.plot(xs_clutterPDAF[:,4], 'b', label='cluttered PDAF')
+ax1.plot(xs_cleanPDAF[:,4], 'c', label='clean PDAF')
 ax1.set(xlabel="Time (s)")
 ax1.set(ylabel="w (rad/s)")
+fig.legend()

P4_PDAF/p4_Classes.py
@@ -97,6 +97,9 @@ class EKF:
         
         # innovation covariance noise covariance matrix S
         self.S = self.H @ self.p_hat @ self.H.T + self.M @ self.R @ self.M.T
+        
+        # enforece symmetry 
+        self.S = 0.5*(self.S + self.S.T)
        
         # Kalman gain matrix
         self.K = self.p_hat @ self.H.T @ np.linalg.inv(self.S)
@@ -105,6 +108,7 @@ class EKF:
         # ------- correct/measurement equations: --------------------------------------------
         # compare expected measurement to sensor measurement
         innovation = y_measured - self.y_hat
+        print(f'EKF innovation: {innovation}')
        
         # posteriori mean state estimate (from apriori state estimate)
         self.x_hat = self.x_hat + self.K @ innovation
@@ -114,13 +118,50 @@ class EKF:
         self.p_hat = 0.5*(self.p_hat + self.p_hat.T) # enforce symmetry
         
 class PDAF(EKF):
-    def __init__(self):
-        super().__init__() 
+    def __init__(self, x0, P0):
+        super().__init__(x0, P0) 
         
         # define additional statistics for PDAF
         self.Pd = 0.9 # probability of detection
         self.gamma_c = 0.0032 # Poisson clutter spatial density
         self.Pg = 0.95 # gate probability
+        self.gate_criteria = chi2.ppf(self.Pg, df=2)
+        
+    def runPDAF(self, dt, ranges, bearings):
+        # input:
+        #   ekf: already initialized ekf class
+        #   dt: time step
+        #   ranges: data series of ranges for time step. Each column is a detection
+        #   bearings: data series of bearings. Each  column is a detection
+        # output:
+        #   state vector at current time step
+        
+        # call EKF with nearest neighbor association
+        # run EKF propogation and measurement matrix calculation
+        self.update_predict_matrices(dt)
+        self.predict(dt)
+        self.update_measurement_matrices()
+        
+        # sort measurements into rows of measurement vector pairs to pass into NN
+        ys = np.array([ranges.to_numpy(),bearings.to_numpy()]).T
+        
+        # find measurements inside gate
+        ys_gated = self.gate(ys)
+        
+        # if >= 1 gated measurement, run measurement update
+        if ys_gated.shape[0] >= 1:
+            self.measurement_correct(ys_gated)
+            
+        return self.x_hat
+        
+    def gate(self, ys_measured):
+        # given a 2d array of detection vectors ys_measured,
+        # returns all detections inside gating criteria according to mahalanobis distance
+        ys_gated = np.empty((0,2))
+        for y in ys_measured:
+            if (y-self.y_hat).T @ np.linalg.inv(self.S) @ (y-self.y_hat) < self.gate_criteria:
+                ys_gated = np.vstack((ys_gated,y))
+        return ys_gated
     
     def measurement_correct(self, ys_measured):
         # ys_measured is already gated
@@ -131,9 +172,9 @@ class PDAF(EKF):
         data_association_probabilities = np.zeros(ys_measured.shape[0])
         likelihoods = np.zeros(ys_measured.shape[0])
         i = 0
+        c = 1/self.gamma_c
+        measurement_gaussian = stats.multivariate_normal(mean=self.y_hat, cov=self.S)
         for y in ys_measured:
-            c = 1/self.gamma_c
-            measurement_gaussian = stats.multivariate_normal(mean=self.y_hat, cov=self.S)
             likelihoods[i] = c*self.Pd*measurement_gaussian.pdf(y) # FIXME check this
             i += 1
             
@@ -157,6 +198,7 @@ class PDAF(EKF):
         
         # posteriori mean state estimate
         self.x_hat = self.x_hat + self.K @ innovation
+        print(f'pdaf innovation: {innovation}')
         
         # calculate uncertainty in which measurement is correct for covariance update
         measurement_uncertainty_sum = 0
@@ -168,10 +210,13 @@ class PDAF(EKF):
             i += 1
             
         # update apriori covariance to posteriori covariance 
+        ekfp_hat = self.p_hat - self.K @ self.S @ self.K.T # FIXME stolen from ekf
+        
+        # FIXME error is here; overriding P to ekf_phat makes pdaf function
         self.p_hat = ((1 - no_target_probability)*(self.p_hat - self.K @ self.S @ self.K.T)
                     + no_target_probability * self.p_hat
-                    + self.K @ (measurement_uncertainty_sum) @ self.K.T)
-        
+                    + self.K * (measurement_uncertainty_sum) @ self.K.T)
+        self.p_hat = ekfp_hat
         # enforce symmetry
         self.p_hat = 0.5*(self.p_hat + self.p_hat.T) 
         